b05450ad96cc28fd061a70c0922ce88ed1331fca
KellyKiiru <infowithkiiru@gmail.com>
2023-05-24 16:05:14 +0300

    commit out unnecessary information

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -36,8 +36,7 @@ const Contact = () => {
             <p className="option-number">01</p>
             <h3 className="contact-how">E-mail</h3>
             <p className="greyish-2">
-              I will reply as soon as possible. Guarantee a response within
-              hours!
+              
             </p>
             <a href="mailto:infowithkiiru@gmail.com" className="gmail">
             infowithkiiru@gmail.com
@@ -47,43 +46,43 @@ const Contact = () => {
             <p className="option-number">02</p>
             <h3>Linked In</h3>
             <p className="greyish-2">
-              You can get in touch with me on my LinkedIN profile. I will reply
+              {/* You can get in touch with me on my LinkedIN profile. I will reply
               to you as soon as possible. The link to my profile is in the top
-              right corner of this page.
+              right corner of this page. */}
             </p>
             <a href="https://www.linkedin.com/in/kelly-kiiru-15a852231/" target="_blank" className="gmail">
             Kelly Kiiru
             </a>
           </div>
-          <div>
+          {/* <div>
             <p className="option-number">03</p>
             <h3>Contact Form</h3>
             <p className="greyish-2">
               You can fill the simple contact form below and send me an email
               directly without you having to open your mail application.
             </p>
-          </div>
+          </div> */}
         </div>
       </div>
       <div className="contact-bottom">
         <div className="flex align-center space-between">
-          <h3>Contact Form</h3>
+          {/* <h3>Contact Form</h3> */}
           <div className="contact-bottom-right flex align-center">
             <div>
-              <h1>Start now!</h1>
-              <p className="questions-advice-constructive">
+              {/* <h1>Start now!</h1> */}
+              {/* <p className="questions-advice-constructive">
                 Questions ,advice , constructive criticism and comments are
                 important to me, so please us the form below to contact me. I
                 will reply as soon as possible. Guaranteed a response within 12
                 hours!
-              </p>
+              </p> */}
             </div>
             <p className="greyish capitalize">
-              LET'S ARRANGE A CONVERSATION ABOUT THE PROJECT.
+              {/* LET'S ARRANGE A CONVERSATION ABOUT THE PROJECT. */}
             </p>
           </div>
         </div>
-        <form className="contact-form flex" onSubmit={handleSubmit}>
+        {/* <form className="contact-form flex" onSubmit={handleSubmit}>
           <div>
             <div className="flex space-between">
               <input type="hidden" name="_captcha" value="false" />
@@ -128,8 +127,8 @@ const Contact = () => {
               <img src={Arrow} alt="Arrow" />
             </div>
           </div>
-        </form>
-        {state.succeeded && (
+        </form> */}
+        {/* {state.succeeded && (
           <>
             <div className="background-transparent" />
             <div className="success-state">
@@ -155,7 +154,7 @@ const Contact = () => {
               </div>
             </div>
           </>
-        )}
+        )} */}
       </div>
     </motion.div>
   );
